7dd5c0a69a363edef59d8ad2d862dd7963dc77c5
Sam <asamadans@gmail.com>
2023-08-02 23:04:32 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+	public static void main(String[] args){
+		Scanner user = new Scanner(System.in);
+		System.out.println("Hello, Wolrd!"+user);
+	}
+}
