01e318226d7d4f2d01352e48d359a438e2f5c3bd
Sam <asamadans@gmail.com>
2023-08-10 00:55:04 +0530

    removed uneccessary line

--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ import java.io.*;
 public class Main{
 	public static void main(String[] args){
 		Scanner user = new Scanner(System.in);
-		System.out.println("Hello, Wolrd!"+user);
+		System.out.println("Hello, "+user.nextLine());
 	}
 }
