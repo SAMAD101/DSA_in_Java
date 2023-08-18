c0d0518d4acf10f3a42a03d9ca012bdc087c680b
Sam <asamadans@gmail.com>
2023-08-18 14:51:10 +0530

    swap

---
+++ Swap.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class Swap {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 9;
+        swap(a, b); // this wont work as java uses pass by value
+        System.out.println(a);
+        System.out.println(b);
+
+        int  arr[] = {1 ,2, 3};
+        change(arr);
+        System.out.println(arr[0]);
+    }
+
+    public static void swap(int a, int b){
+        a = a + b;
+        b = a-b;
+        a = a-b;
+    }
+
+    public static void change(int[] arr){
+        arr[0] = 0;
+    }
+}
