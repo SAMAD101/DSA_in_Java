567bc40b826eb79d2396a0b2fd2edf8695728a87
Sam <asamadans@gmail.com>
2023-08-19 16:21:08 +0530

    linear search

---
+++ LinearSearch.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {1,2,4,332,45,34,9,57};
+        linearSearch(arr, 2);
+    }
+
+    public static void linearSearch(int[] arr, int ele){
+        for(int i=0; i<arr.length; i++) {
+            if (arr[i] == ele) {
+                System.out.println(ele + " found at index " + i);
+            }
+        }
+    }
+}
