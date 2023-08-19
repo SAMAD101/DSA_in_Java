1f4435c2bdf1837cb849a420333cf21a7ddd5a20
Sam <asamadans@gmail.com>
2023-08-19 16:52:37 +0530

    binary search recursively

---
+++ BinarySearch.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,6,8,99};
+        runBinarySearchRecursively(arr, 4, 0, 7);
+    }
+
+    public static int runBinarySearchRecursively(
+            int[] sortedArray, int key, int low, int high) {
+        int middle = low  + ((high - low) / 2);
+
+        if (high < low) {
+            return -1;
+        }
+
+        if (key == sortedArray[middle]) {
+            return middle;
+        } else if (key < sortedArray[middle]) {
+            return runBinarySearchRecursively(
+                    sortedArray, key, low, middle - 1);
+        } else {
+            return runBinarySearchRecursively(
+                    sortedArray, key, middle + 1, high);
+        }
+    }
+}
