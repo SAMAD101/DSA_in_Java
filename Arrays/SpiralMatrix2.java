5983bbd0c2c8c693d5b2b990b48df17ecbc6d042
Sam <asamadans@gmail.com>
2023-09-01 15:17:23 +0530

    spiral

---
+++ Arrays/SpiralMatrix2.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+public class SpiralMatrix2 {
+    public static void main(String[] args) {
+
+    }
+    public static int[][] genMat(int n){
+        int[][] res = new int[n][n];
+
+        for(int i=1; i<=(n*n); i++){
+            
+        }
+
+    }
+}
