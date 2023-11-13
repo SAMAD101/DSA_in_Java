49b81a0612b297df6cec60d2295f33c9c48acce8
Sam <asamadans@gmail.com>
2023-11-14 00:57:22 +0530

    some new problems solved

---
+++ Arrays/Leetcode_219.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class Leetcode_219 {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1};
+        System.out.println(containsNearbyDuplicate(nums, 3));
+    }
+
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<i+k+1 && j<nums.length; j++){
+                if(nums[i]==nums[j] && (j-i)<=k)
+                    return true;
+            }
+
+        }
+        return false;
+    }
+}
