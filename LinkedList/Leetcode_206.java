9da4720cd40a4a5d33d9e2210915a33b7bb07544
Sam <asamadans@gmail.com>
2023-11-14 22:15:57 +0530

    some leetcode problems

---
+++ LinkedList/Leetcode_206.java
@@ -0,0 +1,34 @@
+package LinkedList;
+
+import java.util.List;
+
+public class Leetcode_206 {
+    public static void main(String[] args) {
+        ListNode head = new ListNode();
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(4);
+        ListNode n5 = new ListNode(5);
+        head.next = n1;
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+
+        reverseList(head);
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        while(current!=null){
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+}
+
