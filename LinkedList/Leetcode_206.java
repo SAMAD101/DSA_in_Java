dbb6f149501c93ee523eeb4290c3128062b413d6
Sam <asamadans@gmail.com>
2023-11-14 22:54:15 +0530

    minor modifications

--- LinkedList/Leetcode_206.java
+++ LinkedList/Leetcode_206.java
@@ -4,17 +4,15 @@ import java.util.List;
 
 public class Leetcode_206 {
     public static void main(String[] args) {
-        ListNode head = new ListNode();
-        ListNode n1 = new ListNode(1);
-        ListNode n2 = new ListNode(2);
-        ListNode n3 = new ListNode(3);
-        ListNode n4 = new ListNode(4);
-        ListNode n5 = new ListNode(5);
+        ListNode head = new ListNode(1);
+        ListNode n1 = new ListNode(2);
+        ListNode n2 = new ListNode(3);
+        ListNode n3 = new ListNode(4);
+        ListNode n4 = new ListNode(5);
         head.next = n1;
         n1.next = n2;
         n2.next = n3;
         n3.next = n4;
-        n4.next = n5;
 
         reverseList(head);
     }
