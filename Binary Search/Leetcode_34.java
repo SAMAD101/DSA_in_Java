bff0d729a7f402571ab63221a508e64c71888375
Sam <asamadans@gmail.com>
2023-11-22 23:19:38 +0530

    another problem based on binary search from leetcode

--- Binary Search/Leetcode_34.java
+++ Binary Search/Leetcode_34.java
@@ -1,17 +1,54 @@
 public class Leetcode_34 {
     public static void main(String[] args) {
-
+        int[] nums = {5, 7, 7, 8, 8, 10};
+        System.out.println(searchRange(nums, 7));
     }
 
-    public static int[] searchRange(int[] nums, int target){
-        int start = 0;
-        int end = nums.length-1;
+    public static int[] searchRange(int[] nums, int target) {
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+        return new int[]{first, last};
+    }
 
-        while(start <= end){
-            int mid = (int) (start+end)/2;
-            if(nums[mid] > target) end = mid - 1;
-            else start = mid + 1;
+    private static int findFirst(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int first = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                if (mid == 0 || nums[mid - 1] != target) {
+                    first = mid;
+                    break;
+                } else {
+                    right = mid - 1;
+                }
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
-        return ;
+        return first;
+    }
+
+    private static int findLast(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int last = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                if (mid == nums.length - 1 || nums[mid + 1] != target) {
+                    last = mid;
+                    break;
+                } else {
+                    left = mid + 1;
+                }
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return last;
     }
 }
