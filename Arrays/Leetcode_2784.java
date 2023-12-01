6e55c69d0bd400c0c57e7c05cf91210d3056120d
Sam <asamadans@gmail.com>
2023-12-02 00:01:18 +0530

    a simple question on arrays

--- Arrays/Leetcode_2784.java
+++ Arrays/Leetcode_2784.java
@@ -1,7 +1,32 @@
 package Arrays;
+import java.util.*;
 
 public class Leetcode_2784 {
     public static void main(String[] args) {
+        int[] arr = {1, 3, 2, 3};
+        System.out.println(isGood(arr));
+    }
 
+    public static boolean isGood(int[] nums) {
+        Arrays.sort(nums);
+        if(nums.length==1){
+            return false;
+        }
+        else{
+            int ans=nums.length-1;
+            int count=1;
+            for(int i=0;i<nums.length-1;i++){
+                if(nums[i]==count){
+                    count++;
+                }
+                else{
+                    return false;
+                }
+            }
+            if(count-1==nums[nums.length-1]){
+                return true;
+            }
+        }
+        return false;
     }
 }
